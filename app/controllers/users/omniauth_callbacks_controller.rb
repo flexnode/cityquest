9ac32d2e8b420f4f332f697b7d6b9c4c8fc98058
John Wong <john@flexnode.com>
2011-11-26 19:58:02 +0800

    Redirect to root even when new FB user

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,7 +9,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      redirect_to root_path
     end
   end
 
