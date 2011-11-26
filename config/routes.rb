2b092440ced9690db173a232fdf9fd814dc3c827
TS Lim <tslim@flexnode.com>
2011-11-26 19:11:52 +0800

    Trails controller and simple landing page

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ Cityquest::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 
-  root :to => 'admin/trails#index'
+  root :to => 'trails#index'
 
   namespace :admin do
     root :to => 'trails#index'
