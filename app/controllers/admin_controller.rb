91798beb0f8143b9c0ca2747932e2056709b6455
TS Lim <tslim@flexnode.com>
2011-11-26 22:55:57 +0800

    Skip auth in admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,5 @@
 class AdminController < ApplicationController
   layout "admin"
-  #http_basic_authenticate_with :name => "admin", :password => "admin", :except => :index
+  http_basic_authenticate_with :name => "admin", :password => "admin"
+  skip_before_filter :authenticate_user!
 end
