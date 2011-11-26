56de157f44507f8e36e6d4c86039e44b30af65dc
John Wong <john@flexnode.com>
2011-11-26 15:55:04 +0800

    Initialize Devise (Omniauthable only)

--- config/application.rb
+++ config/application.rb
@@ -45,5 +45,8 @@ module Cityquest
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Configure Host
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   end
 end
