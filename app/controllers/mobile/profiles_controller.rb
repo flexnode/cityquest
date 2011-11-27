ba1a16b21a62996c03a7bcc77eba838d45428bf3
John Wong <john@flexnode.com>
2011-11-27 10:35:25 +0800

    Show nearby location

--- app/controllers/mobile/profiles_controller.rb
+++ app/controllers/mobile/profiles_controller.rb
@@ -4,6 +4,8 @@ class Mobile::ProfilesController < MobileController
   def show
     @trail = Trail.featured.first
     @locations = @trail.locations
+
+    @nearby = Location.near([session[:latitude].to_f, session[:longitude].to_f], 2).first
   end
 
   def quests
