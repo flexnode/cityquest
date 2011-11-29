92761cd74cb658db47350a085d2530826237022b
John Wong <john@flexnode.com>
2011-11-29 12:20:38 +0800

    Store float in session

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,8 +2,8 @@ class Api::UsersController < ApplicationController
   respond_to :json
 
   def update
-    session[:latitude] = params[:latitude]   
-    session[:longitude] = params[:longitude]
+    session[:latitude] = params[:latitude].to_f
+    session[:longitude] = params[:longitude].to_f
     render :nothing => true
   end
 
