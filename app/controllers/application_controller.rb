ebef80152b431a544f46c6a3edaf8bfacd0c5b0d
John Wong <john@flexnode.com>
2011-11-29 12:13:48 +0800

    Fix Devise scoping

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
 
   # Overwriting the sign_out redirect path method
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_in_path_for(resource_or_scope)
     root_path
   end
 
